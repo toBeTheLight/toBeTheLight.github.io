eef7366a8d595e9062c5b470eae04252c74d8d40
toBeTheLight <yl1991web@126.com>
2017-06-17 23:36:40 +0800

    add:swiper.vue

---
+++ demo/imooc/static/script/rem.js
@@ -0,0 +1,12 @@
+(function(doc, win, vWidth) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function() {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            docEl.style.fontSize = 100 * (clientWidth / vWidth) + 'px';
+        };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window, 750);
